fda467b30b04da32110f143dd5a9bf3331084b12
Marcin_Cer <midnajt0@gmail.com>
2022-06-30 12:08:18 +0200

    section 11 done

--- src/components/templates/NewsSection/NewsSection.js
+++ src/components/templates/NewsSection/NewsSection.js
@@ -3,79 +3,63 @@ import { Wrapper, NewsSectionHeader, TitleWrapper, ArticleWrapper, ContentWrappe
 import { Button } from 'components/atoms/Button/Button';
 import axios from 'axios';
 
-const API_TOKEN = '25a5e6035428fef4fcc8217e318f75';
-
-// const data = [
-//   {
-//     title: 'New computers at school',
-//     category: 'Tech news',
-//     content:
-//       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dignissimos ipsum similique amet earum. Error vel quisquam liberopraesentium laborum',
-//   },
-//   {
-//     title: 'New computers at school2',
-//     category: 'Tech news',
-//     content:
-//       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dignissimos ipsum similique amet earum. Error vel quisquam liberopraesentium laborum',
-//     image: 'https://cdn.pixabay.com/photo/2014/09/20/13/52/board-453758_960_720.jpg',
-//   },
-//   {
-//     title: 'New computers at school3',
-//     category: 'Tech news',
-//     content:
-//       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dignissimos ipsum similique amet earum. Error vel quisquam liberopraesentium laborum',
-//     image: 'https://cdn.pixabay.com/photo/2015/01/08/18/24/children-593313_960_720.jpg',
-//   },
-// ];
+export const query = `{
+  allArticles {
+    title
+    category
+    content
+    image {
+      url
+    }
+  }
+}
+`;
 
 const NewsSection = () => {
   const [articles, setArticles] = useState([]);
+  const [error, setError] = useState('');
 
   useEffect(() => {
     axios
       .post(
         'https://graphql.datocms.com/',
         {
-          query: `{
-          allArticles {
-            title
-            category
-            content
-            image {
-              url
-            }
-          }
-        }
-        `,
+          query: query,
         },
         {
           headers: {
-            authorization: `Bearer ${API_TOKEN}`,
+            authorization: `Bearer ${process.env.REACT_APP_DATOCMS_TOKEN}`,
           },
         }
       )
       .then(({ data: { data } }) => {
         setArticles(data.allArticles);
       })
-      .catch((err) => console.log(err));
-  });
+      .catch(() => {
+        setError("Sorry, we couldn't load articles for You");
+      });
+  }, []);
 
   return (
     <Wrapper>
       <NewsSectionHeader>University news feed</NewsSectionHeader>
-      {articles.map(({ title, category, content, image = null }) => (
-        <ArticleWrapper key={title}>
-          <TitleWrapper>
-            <h3>{title}</h3>
-            <p>{category}</p>
-          </TitleWrapper>
-          <ContentWrapper>
-            <p>{content}</p>
-            {image ? <img src={image.url} alt="articleImage" /> : null}
-          </ContentWrapper>
-          <Button isBig>Click me</Button>
-        </ArticleWrapper>
-      ))}
+      {articles.length > 0 ? (
+        articles.map(({ title, category, content, image = null }) => (
+          <ArticleWrapper key={title}>
+            <TitleWrapper>
+              <h3>{title}</h3>
+              <p>{category}</p>
+            </TitleWrapper>
+            <ContentWrapper>
+              <p>{content}</p>
+              {image ? <img src={image.url} alt="articleImage" /> : null}
+            </ContentWrapper>
+            <Button isBig>Click me</Button>
+          </ArticleWrapper>
+        ))
+      ) : (
+        <NewsSectionHeader>{error ? error : 'Loading...'}</NewsSectionHeader>
+      )}
     </Wrapper>
   );
 };
